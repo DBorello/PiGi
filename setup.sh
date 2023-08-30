87f4a8296b63805deab1a695cb03a91ce7fb679a
DBorello <dan@borellos.com>
2023-08-30 07:35:30 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 # Clone repository
 git clone https://github.com/DBorello/PiGi.git
 
+# Enable SPI (TODO Fine dietpi-config way)
+
 pip install boto3
-apt install python3-dev
+
+apt install gcc-arm-linux-gnueabihf
 pip install spidev
