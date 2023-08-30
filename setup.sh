b97d8199218b89db21be9174b9712f6e4bede5a5
DBorello <dan@borellos.com>
2023-08-30 10:25:56 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,12 @@ git clone https://github.com/DBorello/PiGi.git
 
 # Enable SPI (TODO Fine dietpi-config way)
 
-pip install boto3
-
+# Install python dependencies
 apt -y install gcc-arm-linux-gnueabihf python3-pil
-pip install spidev
+pip install spidev boto3
+
+# Install BerryLan
+echo "deb http://repository.nymea.io bullseye rpi" | tee /etc/apt/sources.list.d/nymea.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key A1A19ED6
+apt update
+apt -y install nymea-networkmanager dirmngr
