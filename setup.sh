15ec4210ac615cbad1c84759ff1109cc5aeedd1e
DBorello <dan@borellos.com>
2023-09-16 18:05:03 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,10 @@
-## This is DietPi
-# Enable SPI & bluetooth (TODO Fine dietpi-config way)
-
-# Install BerryLan
-echo "deb http://repository.nymea.io bullseye rpi" | tee /etc/apt/sources.list.d/nymea.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-key A1A19ED6
-apt update
-apt -y install bluez nymea-networkmanager dirmngr 
-
-# Install python dependencies
-apt -y install gcc-arm-linux-gnueabihf python3-pil
-pip install spidev boto3
-
-# Clone repository
-git clone https://github.com/DBorello/PiGi.git
-
 ## Berry Lan
 # Burn BerryLan Image from https://github.com/nymea/berrylan
 # Used bullseye-2023-08-18
 
 # Find using BerryLan iOS App.  Join WiFi network
 # SSH to IP Shown.  Username: nymea Password: nymea
+# Run this command 
 
 # Install dependencies
 sudo apt -y install git python3-pil python3-boto3
