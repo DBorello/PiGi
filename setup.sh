e2e0ed85f163e9e715806c8fbc7464333cedbca5
DBorello <dan@borellos.com>
2023-09-16 18:15:18 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,3 +20,8 @@ echo "dtparam=spi=on" |sudo tee -a /boot/config.txt
 # Configure nymea Berrylane
 sudo sed -i 's/Mode=offline/Mode=always/g' /etc/nymea/nymea-networkmanager.conf
 sudo sed -i 's/AdvertiseName=BT WLAN setup/AdvertiseName=PiGi/g' /etc/nymea/nymea-networkmanager.conf
+
+# Setup service
+sudo cp /home/nymea/PiGi/PiGi.service  /etc/systemd/system/PiGi.service
+sudo chmod 644 /etc/systemd/system/PiGi.service
+sudo systemctl enable PiGi.service
